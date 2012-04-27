11629691928b5752e9a5ee43e943acf081178040
KuMing <KuMing@cogito.co.kr>
2012-04-28 00:48:48 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CogitoHP::Application.config.secret_token = '2f10328c138a2f280a0e9b5a1502b9656414661320603beb3fc77da1a00b196fb370551daeb4075399d311c9d72d47f3893b37cd8d1e10ab90446ae0c3785331'
